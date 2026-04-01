bb4331ab7c4c77ba08ee4c177e2b602fb57a218e
oyidoyid <dionmatthew18@gmail.com>
2026-04-01 20:39:55 +0800

    Update app.py

--- worldbalance_flask - Copy (2)/app.py
+++ worldbalance_flask - Copy (2)/app.py
@@ -446,4 +446,4 @@ def handle_large_file(e):
 # Run
 # -------------------------------------------------
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=10000)
